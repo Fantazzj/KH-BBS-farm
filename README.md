8ed6f6cd789e13d037accaa2ab7c14b9da6c206c
Fantazzj <63418260+Fantazzj@users.noreply.github.com>
2022-01-05 09:39:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# kh-bbs-farm
+ A script for FreePIE to farm unversed in Kingdom Hearts: Birth By Sleep
---
+++ kh-bbs-farm-script.py
@@ -0,0 +1,31 @@
+import time
+
+a = 0
+
+def press(k, t=0.1):
+	keyboard.setKeyDown(k)
+	time.sleep(t)
+	keyboard.setKeyUp(k)
+
+def wait(t):
+	time.sleep(t)
+
+wait(5)
+while True:
+	a+=1
+	press(Key.W,4)
+	press(Key.A,1.5)
+	wait(0.5)
+	press(Key.F)
+	wait(4)
+	press(Key.F)
+	wait(2.5)
+	press(Key.A,1.9)
+	press(Key.W,5)
+	wait(1)
+	press(Key.C)
+	if a==2:
+		wait(20)
+		a=0
+	press(Key.S,2)
+	wait(2)
